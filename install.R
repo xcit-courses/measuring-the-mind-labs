71bede48a22861c1006cabd436fafdc556c5491b
pedro <pdrcardoso@gmail.com>
2026-03-24 08:42:39 +0100

    Update reliability exercises.

--- install.R
+++ install.R
@@ -1,5 +1,4 @@
 # Install required R packages for the course
 # This file is used by Binder to set up the R environment
-install.packages("tidyverse")
-install.packages("ggplot2")
+install.packages("tidyverse", dependencies = TRUE)
 install.packages("knitr")
